4342aae28e87079dc422cf175b08e01214d9669b
Andrew Clarkson <andrew@clarkson.mn>
2015-01-14 14:16:57 -0600

    Add initial header info

---
+++ commercial-reps.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package commercial-reps
+ * @version 0.1
+ */
+
+/*
+Plugin Name: 2nd Wind Commercial Reps
+Plugin URI: github.io/2nd-wind-exercise/commercial-reps
+Description: A simple site-specific plugin to manage 2nd Wind Exercise's Commercial Representatives 
+Author: Andrew Clarkson
+Version: 0.1
+Author URI: andrew.clarkson.mn
+ */
